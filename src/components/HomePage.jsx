ba019559ef9d366ed165e2306e87d004f823d70e
sravya-1605 <asravya384@gmail.com>
2025-08-02 10:52:12 +0530

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -33,7 +33,7 @@ const HomePage = () => {
     <div className="container">
       {/* Header */}
       <div className="header">
-        <h1>E-Commerce</h1>
+        <h1>E-Commerce-klu</h1>
         <div className="header-right">
           {isAuthenticated ? (
             <div className="profile-menu">
@@ -86,7 +86,7 @@ const HomePage = () => {
       </div>
 
       {/* Footer */}
-      <div className="footer">@ copyright E-Commerce-klu</div>
+      <div className="footer">@ copyright E-Commerce</div>
     </div>
   );
 };

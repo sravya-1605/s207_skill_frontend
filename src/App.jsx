70e702c2a59dc0289b3a1ae1897c63cf84f1fa4e
sravya-1605 <asravya384@gmail.com>
2025-08-02 10:44:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce-klu">
+    <BrowserRouter basename="/ecommerce">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
